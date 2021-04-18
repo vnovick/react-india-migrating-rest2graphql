d1856d79c12fb8cafe1fc33467ac99272f48c50b
Vladimir Novick <vnovick@gmail.com>
2021-04-18 23:00:29 +0300

    adding final file

---
+++ api/app.final.js
@@ -0,0 +1,59 @@
+const createError = require('http-errors')
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const jsonServer = require('json-server')
+const cors = require('cors')
+const {ApolloServer, gql} = require('apollo-server-express')
+const {typeDefs} = require('./typeDefs/index.final')
+const {resolvers} = require('./resolvers/final')
+
+const app = express()
+
+///////////Here we will start GraphQL implementation
+
+// Construct a schema, using GraphQL schema language
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  mocks: true,
+  mockEntireSchema: false,
+})
+
+server.applyMiddleware({app})
+
+//////////////// End of GraphQL section
+
+app.use(logger('dev'))
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(cookieParser())
+app.use(cors())
+
+//----------------------------------------------------
+
+// JSON server mocks
+
+app.use('/api', jsonServer.router('db.json'))
+
+//----------------------------------------------------
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404))
+})
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  // render the error page
+  res.status(err.status || 500)
+  res.json(err)
+})
+
+module.exports = app
